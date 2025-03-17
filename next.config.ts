e38935f3648e934bcef52a7066452d32466d68ec
SunnySiddique <devsunnywithcodeing@gmail.com>
2025-03-17 21:42:53 +0600

    next.config updated

--- next.config.ts
+++ next.config.ts
@@ -19,12 +19,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default nextConfig;
